8c0c07138292ccddcd7b2f38d4021bfb384dcd8f
Nick Walker <nick@nickwalker.us>
2019-01-25 14:51:37 -0800

    Add arm files

---
+++ applications/scripts/arm_demo.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+import robot_api
+import rospy
+
+
+def wait_for_time():
+    """Wait for simulated time to begin.
+    """
+    while rospy.Time().now().to_sec() == 0:
+        pass
+
+
+def main():
+    rospy.init_node('arm_demo')
+    wait_for_time()
+    argv = rospy.myargv()
+    DISCO_POSES = [[1.5, -0.6, 3.0, 1.0, 3.0, 1.0, 3.0],
+                   [0.8, 0.75, 0.0, -2.0, 0.0, 2.0, 0.0],
+                   [-0.8, 0.0, 0.0, 2.0, 0.0, -2.0, 0.0],
+                   [-1.5, 1.1, -3.0, -0.5, -3.0, -1.0, -3.0],
+                   [-0.8, 0.0, 0.0, 2.0, 0.0, -2.0, 0.0],
+                   [0.8, 0.75, 0.0, -2.0, 0.0, 2.0, 0.0],
+                   [1.5, -0.6, 3.0, 1.0, 3.0, 1.0, 3.0]]
+
+    torso = robot_api.Torso()
+    torso.set_height(robot_api.Torso.MAX_HEIGHT)
+
+    arm = robot_api.Arm()
+    for vals in DISCO_POSES:
+        arm.move_to_joints(robot_api.ArmJoints.from_list(vals))
+
+
+if __name__ == '__main__':
+    main()
